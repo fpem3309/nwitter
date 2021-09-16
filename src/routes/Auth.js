b8fadee9913aefbc1789b06aa9cc7d69329fb2f4
fpem3309 <alsrhks2154@gmail.com>
2021-09-16 17:17:02 +0900

    input

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,4 +1,36 @@
 import React from "react";
+import { useState } from "react";
 
-const Auth = () => <span>Auth</span>
+const Auth = () => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const onChange = (event) => {
+
+        const { target: { name, value } } = event;
+        console.log(value)
+        if (name === "email") {
+            setEmail(value);
+        } else if (name === "password") {
+            setPassword(value);
+        }
+
+    };
+    const onSubmit = (event) => {
+        event.preventDefault(); //기본행위 실행x
+    };
+
+    return (
+        <div>
+            <form onSubmit={onSubmit}>
+                <input type="email" name="email" placeholder="Email" value={email} onChange={onChange} required />
+                <input type="password" name="password" placeholder="Password" value={password} onChange={onChange} required />
+                <input type="submit" value="Log In" />
+            </form>
+            <div>
+                <button>Continue with Google</button>
+                <button>Continue with Github</button>
+            </div>
+        </div>
+    );
+};
 export default Auth;
