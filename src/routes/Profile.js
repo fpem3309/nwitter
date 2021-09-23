0bfc1aa31f88e315d64fbfcab614f399db844a0d
fpem3309 <alsrhks2154@gmail.com>
2021-09-23 15:57:32 +0900

    sign_out

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,3 +1,11 @@
+import { authService } from "fbase";
 import React from "react";
 
-export default () => <span>Home</span>;
+export default () => {
+    const onLogOutClick = () => authService.signOut();
+    return (
+        <>
+            <button onClick={onLogOutClick}>Log Out</button>
+        </>
+    );
+};
