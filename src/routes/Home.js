7d26d10af3d2dabeb0f7b1fa9af35423bb216fc6
fpem3309 <alsrhks2154@gmail.com>
2021-09-25 17:14:19 +0900

    console_nweets

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,8 +1,17 @@
 import { dbService } from "fbase";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 const Home = () => {
     const [nweet, setNweet] = useState("");
+    const [nweets, setNweets] = useState([]);
+    const getNweets = async () => {
+        const dbNweets = await dbService.collection("nweets").get();
+        dbNweets.forEach(document => console.log(document.data()));
+    }
+
+    useEffect(() => { // component가 mount될때
+        getNweets();
+    }, [])
     const onSubmit = async (event) => {
         event.preventDefault();
         await dbService.collection("nweets").add({
