a78b9cf14728281a6d1f75b8e27696e25af38875
fpem3309 <alsrhks2154@gmail.com>
2021-10-18 16:59:24 +0900

    upload_read

--- src/components/App.js
+++ src/components/App.js
@@ -4,23 +4,18 @@ import { authService } from "fbase";
 
 function App() {
   const [init, setInit] = useState(false); //아직 초기화 x
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [userObj, setUserObj] = useState(null);
   useEffect(() => {
     authService.onAuthStateChanged((user) => {
       if (user) {
-        setIsLoggedIn(true);
         setUserObj(user);
-      } else {
-        setIsLoggedIn(false);
       }
       setInit(true);
     });
   }, [])
   return (
     <>
-      {init ? <AppRouter isLoggedIn={isLoggedIn} userObj={userObj} /> : "Initializing"}
-      <footer>&copy; {new Date().getFullYear()} Home witter</footer>
+      {init ? <AppRouter isLoggedIn={Boolean(userObj)} userObj={userObj} /> : "Initializing"}
     </>
   );
 }
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -8,7 +8,7 @@ const Home = ({ userObj }) => {
     const [nweets, setNweets] = useState([]);
 
     useEffect(() => { // component가 mount될때
-        dbService.collection("nweets").onSnapshot(snapshot => {
+        dbService.collection("nweets").onSnapshot((snapshot) => {
             const nweetsArray = snapshot.docs.map((doc) => ({
                 id: doc.id,
                 ...doc.data(),
@@ -29,10 +29,20 @@ const Home = ({ userObj }) => {
         const { target: { value }, } = event;    // event안의 target안의 value추출
         setNweet(value);
     };
+    const onFileChange = (event) => {
+        const { target: { files }, } = event; //event안에서 target안으로 가서 files를 받아옴
+        const theFile = files[0]; // file을 갖고
+        const reader = new FileReader(); // reader를 만든 다음
+        reader.onloadend = (finishedEvent) => {
+            console.log(finishedEvent);
+        };
+        reader.readAsDataURL(theFile); // 파일을 읽음
+    };
     return (
         <div>
             <form onSubmit={onSubmit}>
                 <input value={nweet} onChange={onChange} type="text" placeholder="What's on your mind?" maxLength={120} />
+                <input type="file" accept="image/*" onChange={onFileChange} />
                 <input type="submit" value="Nweet" />
             </form>
             <div>
