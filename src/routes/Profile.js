25c8f1d23bea5fd2428f62b3bd5395e35afc63c2
fpem3309 <alsrhks2154@gmail.com>
2021-10-27 14:44:28 +0900

    profile_lotto

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -8,8 +8,38 @@ export default () => {
         authService.signOut()
         history.push("/");
     };
+
+    //---------------------로또-----------------------
+
+    var lotto = [];
+
+    for (var i = 0; i < 6; i++) {
+        var lotto_num = Math.floor(Math.random() * 44) + 1;
+
+        for (var j in lotto) {
+            if (lotto_num === lotto[j]) {
+                lotto_num = Math.floor(Math.random() * 44) + 1;
+            }
+        }
+        lotto.push(lotto_num); // push - 배열의 마지막에 값 추가 메소드
+    }
+
+    // 오름차순으로 숫자 정렬
+    lotto.sort(function (a, b) {
+        return a - b;
+    })
+
+    //--------------------로또 끝---------------------
+
     return (
         <>
+
+            <div>
+                <h5> 이번주 로또 1등 번호는
+                    <br />{lotto + " "}로 사보시죠!!!!</h5>
+            </div>
+
+
             <button onClick={onLogOutClick}>Log Out</button>
         </>
     );
