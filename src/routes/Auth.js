21774611b176d46e78db34b5324135c3411618ae
fpem3309 <alsrhks2154@gmail.com>
2021-11-08 16:12:40 +0900

    cleaning

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,40 +1,11 @@
+import AuthForm from "components/AuthForm";
 import { authService, firebaseInstance } from "fbase";
 import React, { useState } from "react";
 
 const Auth = () => {
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const [newAccount, setNewAccount] = useState(true);
-    const [error, setError] = useState("");
-    const onChange = (event) => {
 
-        const { target: { name, value } } = event;
-        console.log(value)
-        if (name === "email") {
-            setEmail(value);
-        } else if (name === "password") {
-            setPassword(value);
-        }
 
-    };
-    const onSubmit = async (event) => {
-        event.preventDefault(); //기본행위 실행x
-        let data;
-        try {
-            if (newAccount) {
-                // account가 true면 새 계정 생성
-                data = await authService.createUserWithEmailAndPassword(email, password);
-            } else {
-                // 아니면 로그인
-                data = await authService.signInWithEmailAndPassword(email, password);
-            }
-        }
-        catch (error) {
-            setError(error.message);
-        }
-        console.log(data);
-    };
-    const toggleAccount = () => setNewAccount(prev => !prev); //이전값을 가져와서 그 값의 반대되는 것을 리턴
+
     const onSocialClick = async (event) => {
         const { target: { name }, } = event;
         let provider;
@@ -49,13 +20,7 @@ const Auth = () => {
 
     return (
         <div>
-            <form onSubmit={onSubmit}>
-                <input type="email" name="email" placeholder="Email" value={email} onChange={onChange} required />
-                <input type="password" name="password" placeholder="Password" value={password} onChange={onChange} required />
-                <input type="submit" value={newAccount ? "Create Account" : "Log In"} />
-                {error}
-            </form>
-            <span onClick={toggleAccount}>{newAccount ? "Sign In" : "Create Account"}</span>
+            <AuthForm />
             <div>
                 <button onClick={onSocialClick} name="google">Continue with Google</button>
                 <button onClick={onSocialClick} name="github">Continue with Github</button>
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,14 +1,10 @@
 import Nweet from "components/Nweet";
-import { v4 as uuidv4 } from "uuid";
 import { dbService, storageService } from "fbase";
 import React, { useEffect, useState } from "react";
+import NweetFactory from "components/NweetFactory";
 
 const Home = ({ userObj }) => {
-    //console.log(userObj);
-    const [nweet, setNweet] = useState("");
     const [nweets, setNweets] = useState([]);
-    const [attachment, setAttachment] = useState("");
-
     useEffect(() => { // component가 mount될때
         dbService.collection("nweets").onSnapshot((snapshot) => {
             const nweetsArray = snapshot.docs.map((doc) => ({
@@ -19,68 +15,15 @@ const Home = ({ userObj }) => {
         });
     }, []);
 
-    const onSubmit = async (event) => {
-        event.preventDefault();
-        let attachmentUrl = "";
-        if (attachment !== "") {
-            const attachmentRef = storageService
-                .ref()
-                .child(`${userObj.uid}/${uuidv4()}`); //이미지의 path, child를 가짐(파일에 대한 reference)
-            const response = await attachmentRef.putString(attachment, "data_url");
-            attachmentUrl = await response.ref.getDownloadURL();
-        }
-        const nweetObj = {
-            text: nweet,
-            createdAt: Date.now(),
-            creatorId: userObj.uid,
-            attachmentUrl,
-        };
-        await dbService.collection("nweets").add(nweetObj);
-        setNweet("");
-        setAttachment("");
-    };
-
-    const onChange = (event) => {
-        const { target: { value }, } = event;    // event안의 target안의 value추출
-        setNweet(value);
-    };
-
-    const onFileChange = (event) => {
-        const { target: { files }, } = event; //event안에서 target안으로 가서 files를 받아옴
-        const theFile = files[0]; // file을 갖고 ( 첫 번째 하나만)
-        const reader = new FileReader(); // reader를 만든 다음
-        reader.onloadend = (finishedEvent) => {
-            const {
-                currentTarget: { result },
-            } = finishedEvent
-            setAttachment(result);
-        };
-        reader.readAsDataURL(theFile); // 파일을 읽음
-    };
-
-    const onClearAttachment = () => setAttachment(null);
-
     return (
         <div>
-            <form onSubmit={onSubmit}>
-                <input value={nweet} onChange={onChange} type="text" placeholder="What's on your mind?" maxLength={120} />
-                <input type="file" accept="image/*" onChange={onFileChange} />
-                <input type="submit" value="Nweet" />
-                {attachment && (
-                    <div>
-                        <img src={attachment} width="50px" height="50px" />
-                        <button onClick={onClearAttachment}>Clear</button>
-                    </div>
-                )}
-            </form>
+            <NweetFactory userObj={userObj} />
             <div>
                 {nweets.map((nweet) => (
                     <Nweet key={nweet.id} nweetObj={nweet} isOwner={nweet.creatorId === userObj.uid} />
                 ))}
             </div>
-
         </div>
-
     );
 };
 export default Home;
